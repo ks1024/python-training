d3a52cb73b29a430de3e08bc7594e6d06ae671a0
yankuangshi <yankuangshi@gmail.com>
2014-07-09 23:51:19 +0200

    add a python project skeleton

---
+++ PackageName/setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.com import setup
+
+config = {
+    'description': 'My project',
+    'author': 'My name',
+    'url': 'URL to get it at.',
+    'download_url': 'Where to download it',
+    'author_email': 'My email',
+    'version': '0.1',
+    'install_requires': ['nose'],
+    'packages': ['NAME'],
+    'scripts': [],
+    'name': 'projectname'
+}
+
+setup(**config)
