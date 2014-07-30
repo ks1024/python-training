573ba527b627e1ac5322e2c4d1df1958aa4c03d2
yankuangshi <yankuangshi@gmail.com>
2014-07-30 23:31:34 +0200

    add color for menu

--- Term-menu/menu.py
+++ Term-menu/menu.py
@@ -8,7 +8,7 @@ def launch_menu():
     curses.noecho()             # Turn off the echo mode
     curses.curs_set(0)          # Leave cursor invisible
     curses.start_color()
-    curses.init_pair(1, curses.COLOR_RED, curses.COLOR_WHITE)  # Initialize a color-pair with RED as fg and WHITE as bg
+    curses.init_pair(1, curses.COLOR_CYAN, curses.COLOR_BLACK)  # Initialize a color-pair with RED as fg and WHITE as bg
     screen.keypad(1)
     selection = -1
     option = 0
@@ -16,15 +16,15 @@ def launch_menu():
     while selection < 0:
         screen.clear()
         h = [0] * 5 # curses.A_NORMAL = 0
-        #h[option] = curses.color_pair(1)
-        h[option] = curses.A_REVERSE
-        screen.addstr(2, 2, "Game Menu")
-        screen.addstr(4, 2, "Please select an option...", curses.A_BOLD)
-        screen.addstr(5, 2, "1 - Play Game", h[0])
-        screen.addstr(6, 2, "2 - Instructions", h[1])
-        screen.addstr(7, 2, "3 - Game Options", h[2])
-        screen.addstr(8, 2, "4 - High Scores", h[3])
-        screen.addstr(9, 2, "5 - Exit ('q')", h[4])
+        h[option] = curses.color_pair(1)
+        #h[option] = curses.A_REVERSE
+        screen.addstr(2, 4, "Game Menu")
+        screen.addstr(4, 4, "Please select an option...", curses.A_BOLD)
+        screen.addstr(5, 4, "1 - Play Game", h[0])
+        screen.addstr(6, 4, "2 - Instructions", h[1])
+        screen.addstr(7, 4, "3 - Game Options", h[2])
+        screen.addstr(8, 4, "4 - High Scores", h[3])
+        screen.addstr(9, 4, "5 - Exit ('q')", h[4])
         screen.refresh()
         q = screen.getch()
         if q == curses.KEY_UP or q == ord('k'):      # KEY_UP or 'k' on vim mode
